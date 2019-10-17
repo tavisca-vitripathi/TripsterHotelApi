6ac4cefad98ad1d86c63527987876e74a29e61e5
Vikas Tripathi <vitripathi@affinionds.com>
2019-10-17 15:49:36 +0530

    first commit

---
+++ Tavisca.Tripster.HotelApi/Tavisca.Tripster.Core/Registry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tavisca.Tripster.Core
+{
+    class Registry
+    {
+    }
+}
---
+++ Tavisca.Tripster.HotelApi/Tavisca.Tripster.Data/Registry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tavisca.Tripster.Data
+{
+    class Registry
+    {
+    }
+}
